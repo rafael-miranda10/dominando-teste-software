f0cf55e7be0ecec2e01073a6f93b853967cc84bb
Rafael Miranda <rafael.miranda@db1.com.br>
2020-06-11 12:09:08 -0300

    [DEV] Features Testes

---
+++ TestesDeSoftware/01 - Testes de Unidade/Features.Tests/01 - Traits/ClienteTests.cs
@@ -0,0 +1,53 @@
+﻿using Features.Clientes;
+using System;
+using Xunit;
+
+namespace Features.Tests._01___Traits
+{
+    public class ClienteTests
+    {
+        [Fact(DisplayName = "Novo Cliente Válido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Rafael",
+                "Arthur",
+                DateTime.Now.AddYears(-30),
+                "rafa@rafa.com",
+                true,
+                DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert 
+            Assert.True(result);
+            Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+        }
+
+        [Fact(DisplayName = "Novo Cliente Inválido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            // Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "rafa2rafa.com",
+                true,
+                DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert 
+            Assert.False(result);
+            Assert.NotEqual(0, cliente.ValidationResult.Errors.Count);
+        }
+    }
+}
---
+++ TestesDeSoftware/01 - Testes de Unidade/Features.Tests/02 - Fixtures/ClienteTestsFixture.cs
@@ -0,0 +1,47 @@
+﻿using Features.Clientes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Features.Tests._02___Fixtures
+{
+    [CollectionDefinition(nameof(ClienteCollection))]
+    public class ClienteCollection : ICollectionFixture<ClienteTestsFixture>
+    { }
+
+    public class ClienteTestsFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Rafael",
+                "Arthur",
+                DateTime.Now.AddYears(-30),
+                "rafa@rafa.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public Cliente GerarClienteInValido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "rafa2rafa.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
