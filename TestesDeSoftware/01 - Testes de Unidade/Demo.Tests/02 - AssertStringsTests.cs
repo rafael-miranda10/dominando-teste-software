242dd4f1e5721b14a2f73e5af5c10e5bb43d1581
Rafael Miranda <rafael.miranda@db1.com.br>
2020-06-11 11:28:35 -0300

    [DEV] Testes Basicos

---
+++ TestesDeSoftware/01 - Testes de Unidade/Demo.Tests/02 - AssertStringsTests.cs
@@ -0,0 +1,92 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertStringsTests
+    {
+        [Fact]
+        public void StringsTools_UnirNomes_RetornarNomeCompleto()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Rafael", "Arthur");
+
+            // Assert
+            Assert.Equal("Rafael Arthur", nomeCompleto);
+        }
+
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveIgnorarCase()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Rafael", "Arthur");
+
+            // Assert
+            Assert.Equal("RAFAEL ARTHUR", nomeCompleto, true);
+        }
+
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveConterTrecho()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Rafael", "Arthur");
+
+            // Assert
+            Assert.Contains("fael", nomeCompleto);
+        }
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveComecarCom()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Rafael", "Arthur");
+
+            // Assert
+            Assert.StartsWith("Rafa", nomeCompleto);
+        }
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_DeveAcabarCom()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Rafael", "Arthur");
+
+            // Assert
+            Assert.EndsWith("thur", nomeCompleto);
+        }
+
+
+        [Fact]
+        public void StringsTools_UnirNomes_ValidarExpressaoRegular()
+        {
+            // Arrange
+            var sut = new StringsTools();
+
+            // Act
+            var nomeCompleto = sut.Unir("Rafael", "Arthur");
+
+            // Assert
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", nomeCompleto);
+        }
+    }
+}
---
+++ TestesDeSoftware/01 - Testes de Unidade/Demo.Tests/07 - AssertingCollectionsTests.cs
@@ -0,0 +1,58 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertingCollectionsTests
+    {
+        [Fact]
+        public void Funcionario_Habilidades_NaoDevePossuirHabilidadesVazias()
+        {
+            // Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Rafael", 10000);
+
+            // Assert
+            // no lugar de for, utlizar o all e passar a expressão para validar cada item
+            Assert.All(funcionario.Habilidades, habilidade => Assert.False(string.IsNullOrWhiteSpace(habilidade)));
+        }
+
+        [Fact]
+        public void Funcionario_Habilidades_JuniorDevePossuirHabilidadeBasica()
+        {
+            // Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Rafael", 1000);
+
+            // Assert
+            Assert.Contains("OOP", funcionario.Habilidades);
+        }
+
+
+        [Fact]
+        public void Funcionario_Habilidades_JuniorNaoDevePossuirHabilidadeAvancada()
+        {
+            // Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Rafael", 1000);
+
+            // Assert
+            Assert.DoesNotContain("Microservices", funcionario.Habilidades);
+        }
+
+
+        [Fact]
+        public void Funcionario_Habilidades_SeniorDevePossuirTodasHabilidades()
+        {
+            // Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Rafael", 15000);
+
+            var habilidadesBasicas = new[]
+            {
+                "Lógica de Programação",
+                "OOP",
+                "Testes",
+                "Microservices"
+            };
+
+            // Assert
+            Assert.Equal(habilidadesBasicas, funcionario.Habilidades);
+        }
+    }
+}
